e6e949692219a96312761b59553fbbba5215ea9f
Your Name <slackbabbath@gmail.com>
2012-01-12 23:06:55 -0800

    Creating home page and adding events to it

--- northrup/views.py
+++ northrup/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.template import RequestContext
+from django.core import serializers
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from django.utils import simplejson
+from mezzanine.utils.views import paginate
+from northrup.models import Event
+import logging
+
+logger = logging.getLogger("westernnorthrup.custom")
+
+def home(request):
+    try:
+        events = Event.objects.all().order_by('-date')
+        return render_to_response('index.html',{'events': events,},
+                                  context_instance=RequestContext(request))
+    except Exception, e:
+        logger.error('Error in view: about: {0}'.format(e))
